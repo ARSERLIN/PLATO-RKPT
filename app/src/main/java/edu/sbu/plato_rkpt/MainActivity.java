9a850ff11fc48a782bece483c3478286a5f14b0f
MHA1380 <mh.arsalan80@outlook.com>
2020-07-15 20:34:32 +0430

    first commit

---
+++ app/src/main/java/edu/sbu/plato_rkpt/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.sbu.plato_rkpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
